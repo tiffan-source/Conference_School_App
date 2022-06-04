c0a61817c491e49967b314a360756a1c571f4aa1
Tiffane <zohoungbogboanlyou@gmail.com>
2022-06-04 21:50:50 +0100

    current create log page

---
+++ Views/create_conference.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/Bases/reset.css">
+    <link rel="stylesheet" href="Style/Bases/base.css">
+    <link rel="stylesheet" href="Style/Modules/links.css">
+    <link rel="stylesheet" href="Style/Modules/title.css">
+    <link rel="stylesheet" href="Style/Modules/input.css">
+    <link rel="stylesheet" href="Style/Modules/button.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="content-app">
+        <?php include("panel.php") ?>
+        
+        <div id="create-conference">
+
+        </div>
+    </div>
+</body>
+</html>
