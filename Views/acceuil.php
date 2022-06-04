1e93d6834fa006bbe200af6d972e482be07687a0
Tiffane <zohoungbogboanlyou@gmail.com>
2022-06-05 00:39:34 +0100

    page acceuil

---
+++ Views/acceuil.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/Bases/reset.css">
+    <link rel="stylesheet" href="Style/Bases/base.css">
+    <link rel="stylesheet" href="Style/Modules/title.css">
+    <link rel="stylesheet" href="Style/Modules/input.css">
+    <link rel="stylesheet" href="Style/Modules/links.css">
+    <link rel="stylesheet" href="Style/Modules/acceuil.css">
+    <link rel="stylesheet" href="Style/Layouts/acceuil.layouts.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div id="content-app">
+        <?php include("panel.php") ?>
+
+        <div id="acceuil">
+            <div class="acceuil-head">
+                <h1 class="acceuil-title">Acceuil</h1>
+                <input type="search" class="input" placeholder="Search">
+            </div>
+            <div class="acceuil-conference">
+                <ul class="acceuil-list-conference">
+                    <li  class="acceuil-list-conference-element">
+                        <div class="acceuil-list-conference-title">
+                            <span class="acceuil-list-conference-name">Conference #1</span>
+                            <span class="acceuil-list-conference-lastupdate">Last update date</span>
+                        </div>
+                        <span class="acceuil-list-conference-status">Status</span>
+                        <span class="acceuil-list-conference-creator">Creator</span>
+                        <a href="#" class="acceuil-list-conference-showmore">Voir plus</a>
+                    </li>
+                    <li  class="acceuil-list-conference-element">
+                        <div class="acceuil-list-conference-title">
+                            <span class="acceuil-list-conference-name">Conference #1</span>
+                            <span class="acceuil-list-conference-lastupdate">Last update date</span>
+                        </div>
+                        <span class="acceuil-list-conference-status">Status</span>
+                        <span class="acceuil-list-conference-creator">Creator</span>
+                        <a href="#" class="acceuil-list-conference-showmore">Voir plus</a>
+                    </li>
+                    <li  class="acceuil-list-conference-element">
+                        <div class="acceuil-list-conference-title">
+                            <span class="acceuil-list-conference-name">Conference #1</span>
+                            <span class="acceuil-list-conference-lastupdate">Last update date</span>
+                        </div>
+                        <span class="acceuil-list-conference-status">Status</span>
+                        <span class="acceuil-list-conference-creator">Creator</span>
+                        <a href="#" class="acceuil-list-conference-showmore">Voir plus</a>
+                    </li>
+                    <li  class="acceuil-list-conference-element">
+                        <div class="acceuil-list-conference-title">
+                            <span class="acceuil-list-conference-name">Conference #1</span>
+                            <span class="acceuil-list-conference-lastupdate">Last update date</span>
+                        </div>
+                        <span class="acceuil-list-conference-status">Status</span>
+                        <span class="acceuil-list-conference-creator">Creator</span>
+                        <a href="#" class="acceuil-list-conference-showmore">Voir plus</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
