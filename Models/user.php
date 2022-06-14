ea1b19e65446dbf6c30a8e2475c9a77a15e04a71
ADEDE Ezéchiel <adedeezechiel@gmail.com>
2022-06-14 11:26:47 +0100

    UserModel

--- Models/user.php
+++ Models/user.php
@@ -1,146 +1,46 @@
 <?php
 	require("Connection.php");
-	
-	class user {
-	       private  $con;
-		private $c;
-		private $id;
-		private $username;
-		private $password;
-		
-		
-		
-		
-		
-		public function __construct()
-		
-  {
-   
-    $ctp = func_num_args();
-   
-    $args = func_get_args();
-   
-        
-    switch($ctp)
-    {
-        case 3:
-           
-                       $this->con = new Connection();
-			$this->c = $this->con->getConnection();
-			$this->id =$args[0];
-			$this->username =$args[1];
-			$this->password = $args[2];
-			
-            break;
-         case 1:
-                       $this->con = new Connection();
-			$this->c = $this->con->getConnection();
-			$this->id =$args[0];
-			$this->username ="";
-			$this->password = "";   
-           
-            break;
-        
-         default:
-            break;
-    }
-}
 
+	class User{
+		public $id;
+		public $username;
+		public $password;
 		
-		
-		
-		
-		 function getId(){
-			return $this->id;
+		public function __construct($id = null, $username = null, $password = null){
+			
+			$this->id = $id;
+			$this->username = $username;
+			$this->password = $password;
 		}
-		
-		
 	    
-	   function getUsername(){
-			return $this->username;
-		}
-		
-		function getPassword(){
-			return $this->password;
-		}
-		
-		
-		
+		/**
+		 * All Getter
+		 */
+
+		/*
 	
-		public function createUser(){
+		*/
+
+		/**
+		 * CRUD
+		 */
 		
-			try{
-				$query = "insert into user  values ($this->id,'$this->username',md5('$this->password') );";
+		static public function checkUser($username,$password){
+			$new_connection = new Connection();
+
+			$query = "SELECT * FROM user where username=?";
+
+			$preparedStatement = $new_connection->getConnection()->prepare($query);
+            $preparedStatement->execute([$username]);
+			$user=$preparedStatement->fetchAll();
+
+			var_dump($user[0]);
+
+			if ($user[0] && $user[0]['password']==$password  )
+				return $user[0]['id'];
 			
-				$r = $this->c->exec ($query);
-				
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
-		}
-		
-			public function deleteUser(){
-		
-			try{
-				
-				$query = "delete from user where id =$this->id;";
-			
-				$r = $this->c->exec ($query);
-				
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
-		}
-		
-			public function updateUserByUsername($username){
-		
-			try{
-				
-				$query = "update user set username='$username' where id =$this->id;";
-			
-				$r = $this->c->exec ($query);
-				
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
-		}
-		
-		public function updateUserByPasssword($password){
-		
-			try{
-			
-				
-				$query = "update user set password=md5('$password') where id =$this->id;";
-			
-				$r = $this->c->exec ($query);
-				
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
-		}
-		
-		
-		
-		
-		
-			public function readUser(){
-		
-			try{
-				
-				$query = "select  * from user where id =$this->id;";
-			
-				$r = $this->c->query ($query);
-				
-				if ($donnees = $r->fetch())
-				 return "Nom d'utilisateur : ". $donnees['username'] ;
-				       
-				       
-				
-			} catch (PDOException $e) {
-				$e->getMessage();
-			}
+
+			return  false;
 		}
 		
 	}
-
-?>
