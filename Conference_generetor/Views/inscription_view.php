85ccfd557082a605aa39abd8edb9822d261c26ee
leClo <mahougnon.yanclo@gmail.com>
2022-06-30 18:10:23 +0100

    mv views

---
+++ Conference_generetor/Views/inscription_view.php
@@ -0,0 +1,28 @@
+<DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8">
+         <title>Inscription</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend> <strong>Enroll</strong> </legend>
+            <label for="last name">Nom</label>
+            <input type="text" name="Nom" >
+            <br>
+            <label for="first name">Prenoms</label>
+            <input type="text" name="Prenom" >
+            <br>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="Age" >
+            <br>
+            <label for="Email">Email</label>
+            <input type="email" name="" >
+            <br>
+            <label for="ville_provenance">Ville de provenance</label>
+            <input type="text" name="" >
+            <br>
+            <button>Submit</button>
+        </fieldset>
+    </body>
+</html>
---
+++ Conference_generetor/Views/publication_view.php
@@ -0,0 +1,14 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset ="UTF-8">
+        <title>Publication</title>
+    </head>
+    <body>
+        <div>
+            <h3>Publication_nom</h3>
+            <p>1. Suite de signes linguistiques constituant un écrit ou une œuvre. Texte corrompu, incomplet, interpolé, mutilé; texte autographe, imprimé, manuscrit, polycopié; texte primitif; texte d'une conférence, d'un discours; altération, interpolation, lacune d'un texte; variantes d'un texte; écrire, éditer, revoir, taper un texte; corriger, retaper un texte; se reporter au texte; étude, critique d'un texte; citer, communiquer, connaître un texte. Je revis le texte si particulier de cette offre d'emploi: « Personne jeune... bonne éducation... célibataire... envoyer photographie » (Duhamel, Confess. min., 1920, p. 127).Les diseurs de profession presque toujours me sont insupportables, qui prétendent faire valoir, interpréter, quand ils surchargent, débauchent 
+            les intentions, altèrent les harmonies d'un texte (Valéry, Variété II, 1929, p. 177).</p>
+        </div>
+    </body>
+</html>
