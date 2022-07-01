18f662075bef91530b9146a8fbc41bcf43f4217c
Tiffane <zohoungbogboanlyou@gmail.com>
2022-07-01 09:40:40 +0100

    sitegen v1.3

--- Conference_generetor/Views/header_view.php
+++ Conference_generetor/Views/header_view.php
@@ -2,12 +2,11 @@
 <header>
     <nav >
         <ul >
-            <li ><a href="#">Acceuil</a></li>
-            <li ><a href="#">Programme</a></li>
-            <li ><a href="#">Publication</a></li>
-            <li ><a href="#">Appel a candidature</a></li>
-            
-            <li ><a href="#">Inscription</a></li>
+            <li ><a href="index.php">Acceuil</a></li>
+            <li ><a href="index.php?action=activite_view">Programme</a></li>
+            <li ><a href="index.php?action=publication_view">Publication</a></li>
+            <li ><a href="index.php?action=appel_candidature_view">Appel a candidature</a></li>
+            <li ><a href="index.php?action=inscription_view">Inscription</a></li>
         </ul>
         <div>
             <h1>THE PROS AVENGERS 2025</h1>
